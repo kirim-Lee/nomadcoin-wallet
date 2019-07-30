d26b1d56b820be84a062ad8f6ece1bd9ebd1ca3a
kirim-Lee <uthi1004@gmail.com>
2019-07-31 00:36:29 +0900

    merge nomadcoin projects

---
+++ index.js
@@ -0,0 +1,48 @@
+const electron = require("electron");
+const path = require("path"),
+  url = require("url");
+
+
+const {
+  app,
+  BrowserWindow
+} = electron;
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    title: "Nomadcoin Wallet"
+  });
+
+  const ENV = process.env.ENV;
+
+  if (ENV && ENV.trim() === "dev") {
+    console.log("dev is");
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    console.log("build is");
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "build/index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow === null;
+  });
+}
+
+
+app.on(
+  "window-all-closed",
+  (process.platform !== "darwin" && app.quit) || null
+);
+app.on("activate", (mainWindow === null && createWindow) || null);
+app.on("ready", createWindow);
--- main.js
+++ main.js
@@ -1,46 +1,11 @@
-const electron = require("electron"),
-  path = require("path"),
-  url = require("url");
+const getPort = require("get-port"),
+  nomadcoin = require("./nomadcoin/src/server.ts");
 
-const {
-  app,
-  BrowserWindow
-} = electron;
 
-let mainWindow = null;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    resizable: false,
-    title: 'Nomadcoin Wallet'
+getPort().then(port => {
+  const server = nomadcoin.app.listen(port, () => {
+    console.log(`running blockcahin node on: http://localhost:${port}`);
   });
-
-  const ENV = process.env.ENV;
-
-  console.log('env:', ENV);
-  if (ENV.trim() === 'dev') {
-    console.log('dev is')
-    mainWindow.loadURL('http://localhost:3000');
-  } else {
-    console.log('build is')
-    mainWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, "build/index.html"),
-        protocol: "file",
-        slashes: true
-      })
-    );
-  }
-
-
-
-  mainWindow.on('closed', () => {
-    mainWindow === null;
-  })
-}
-
-app.on('window-all-closed', process.platform !== 'darwin' && app.quit || null);
-app.on('activate', mainWindow === null && createWindow || null);
-app.on("ready", createWindow);
+  nomadcoin.startP2PServer(server);
+});
