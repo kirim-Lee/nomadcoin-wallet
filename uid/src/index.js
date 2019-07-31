fddfe75f25af9e79bb33512a8b473d898f414092
kirim-Lee <uthi1004@gmail.com>
2019-07-31 22:33:28 +0900

    shareing variable between electron and react

--- uid/src/index.js
+++ uid/src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { remote } from "electron";
+import { remote } from 'electron';
 
-declare global {
-  interface Window {
-    sharedPort: any;
-  }
-}
 
 const sharedPort = remote.getGlobal("sharedPort");
 
 alert(sharedPort);
-
 window.sharedPort = sharedPort;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
